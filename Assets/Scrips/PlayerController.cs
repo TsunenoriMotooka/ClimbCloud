2ee661448c9f1bb3d9e456ed3296aa3457e3d1db
TsunenoriMotooka <tsune.motooka@gmail.com>
2025-04-08 20:45:34 +0900

    バグ対応

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -21,7 +22,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
         {
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
@@ -42,6 +43,11 @@ public class PlayerController : MonoBehaviour
         }
 
         this.animator.speed = speedx / 2.0f;
+
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collisione)
