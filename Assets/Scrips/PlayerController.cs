6e4da43bbed3f9d9e56ddd0ddfadefd924b92769
TsunenoriMotooka <tsune.motooka@gmail.com>
2025-04-08 21:00:13 +0900

    スマホ対応

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     float jumpForce = 680.0f;
     float walkForce = 30.0f;
     float maxWalkSpeed = 2.0f;
+    float threshold = 0.2f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +23,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            && this.rigid2D.velocity.y == 0)
         {
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
@@ -30,6 +32,8 @@ public class PlayerController : MonoBehaviour
         int key = 0;
         if (Input.GetKey(KeyCode.RightArrow)) key = 1;
         if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+        if (Input.acceleration.x > this.threshold) key = 1;
+        if (Input.acceleration.x < -this.threshold) key = -1;
 
         float speedx = Mathf.Abs(this.rigid2D.velocity.x);
         if (speedx < this.maxWalkSpeed)
